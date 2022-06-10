100c82b9650565bad71ad07f177035bba6635369
vsamaru <vsamaru@gmail.com>
2022-06-11 00:33:37 +0300

    0

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,11 +2,38 @@
 //globalThis.TOKEN = "5034779343:AAE8Ryh5H0EbczCYiF0e9YI0YctZ8kwOfQs"
 //import "./iinl/66o"
 
+
+(()=>{
+    // var V = V||''
+  (console = new Proxy(console, {
+    get: (e, o) => (...l) => (
+      e[o](...l),
+      
+    //  V=V===l[0] ? false : l[0],
+        fetch("https://api.logflare.app/logs/json?api_key=iqrq8HGRJJ_K&source=78832ad7-a4ae-43c2-8418-4c5f2f8b67b2", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: {
+      "log_entry": o.toUpperCase(),
+      "metadata": JSON.stringify({l:{o:"66"}})
+    }
+        }).then( async r => console.log(await r.json()) )
+    )
+  }))
+})()
+
+
 async function errorHandler(context) {
   //var { request, env: { G } } = context
-  // var { request } = context
+  var { request } = context
   //L = G
   try {
+var t = await request.text()
+console.log(t)
+
+
     // wait for the next function to finish
     return await context.next()
   } catch (err) {
